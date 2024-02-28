52cb5153f40fb474ecf7ab706c120607685e3621
unknown <mrahamdjedcoach@gmail.com>
2024-02-28 21:57:56 +0100

    set the struct

---
+++ lib/src/core/constant/ScColor.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ScColor{
+  static const color_bg = Colors.black;
+  static const color_1 = Color(0xFF18181a);
+  static const color_2 = Color(0xFF101012);
+}
