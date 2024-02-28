c015cf7bc32f3487db86a59097dd787530168570
unknown <mrahamdjedcoach@gmail.com>
2024-02-28 23:25:57 +0100

    values changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:chess_timer/src/core/constant/ScFont.dart';
+import 'package:chess_timer/src/core/constant/ScSize.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +12,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return;
   }
 }
+
+// class timer extends StatelessWidget {
+//   const timer({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: const BoxDecoration(
+//         borderRadius: BorderRadius.only(topLeft: Radius.circular(ScSize.radius_meduim),topRight : Radius.circular(ScSize.radius_meduim)
+//         ),
+//       ),
+//       child: Column(
+//         children: [
+//           AutoSizeText("Moves : 16",style: ScFont.getH4(),minFontSize: ScFont.h4Min,),
+//           AutoSizeText("",style: ScFont.getH1(),minFontSize: ScFont.h1Min,),
+//           AutoSizeText("Moves : 16",style: ScFont.getH3(),minFontSize: ScFont.h3Min,),
+//         ],
+//       ),
+//     );
+//   }
+// }
