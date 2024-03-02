566ea939a79aff9358f58b4778c2b0364be95618
unknown <mrahamdjedcoach@gmail.com>
2024-03-02 23:41:05 +0100

    pub and main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:chess_timer/src/core/constant/ScFont.dart';
-import 'package:chess_timer/src/core/constant/ScSize.dart';
+import 'package:chess_timer/src/core/constant/sc_font.dart';
+import 'package:chess_timer/src/core/constant/sc_size.dart';
+import 'package:chess_timer/src/screens/home/pages/home_screen.dart';
+import 'package:chess_timer/src/screens/home/pages/settings_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return;
+    return const MaterialApp(
+      home: SettingsScreen(),
+    );
   }
 }
 
