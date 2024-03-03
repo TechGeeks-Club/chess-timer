6cb57ad12574ef1949a73154300683c92bc3b6ed
unknown <mrahamdjedcoach@gmail.com>
2024-03-03 13:16:46 +0100

    merge main branch with ui

--- lib/main.dart
+++ lib/main.dart
@@ -19,26 +19,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class timer extends StatelessWidget {
-//   const timer({
-//     super.key,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: const BoxDecoration(
-//         borderRadius: BorderRadius.only(topLeft: Radius.circular(ScSize.radius_meduim),topRight : Radius.circular(ScSize.radius_meduim)
-//         ),
-//       ),
-//       child: Column(
-//         children: [
-//           AutoSizeText("Moves : 16",style: ScFont.getH4(),minFontSize: ScFont.h4Min,),
-//           AutoSizeText("",style: ScFont.getH1(),minFontSize: ScFont.h1Min,),
-//           AutoSizeText("Moves : 16",style: ScFont.getH3(),minFontSize: ScFont.h3Min,),
-//         ],
-//       ),
-//     );
-//   }
-// }
