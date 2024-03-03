53e875b3245d571dc5d2388d005be1d7c6db9f60
unknown <mrahamdjedcoach@gmail.com>
2024-03-03 10:22:39 +0100

    add the splach screen and assets

--- lib/src/screens/splash_screen.dart
+++ lib/src/screens/splash_screen.dart
@@ -1,4 +1,4 @@
-import 'package:chess_timer/src/core/constant/ScSize.dart';
+import 'package:chess_timer/src/core/constant/sc_size.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
